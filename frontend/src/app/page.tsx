59c10dce2d73db3122540837dd027041fd1005ae
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-28 09:26:16 -0800

    project

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,8 @@ import Card from '../Components/Card'
 import Footer from '../Components/Footer'
 import TrandingCourse from '@/Components/TrandingCourse'
 import Addmission from '../Components/Addmission'
+import Faq from '../Components/Faq'
+import Faculty from '../Components/Faculty'
 function page() {
   return (
     <>
@@ -14,8 +16,10 @@ function page() {
    <TrandingCourse/>
     <Card/>
     <TrandingCourse/>
+    <Faq/>
     <Card/>
     <Addmission/>
+    <Faculty/>
     <Footer/>
     </>
   )
